d4a93b67d33c58bf7da11ea216dc1cab865c94dd
idevtier <idevtier@gmail.com>
2022-07-19 15:49:29 +0200

    Using empty object instead Empty

--- application/grpc-crud-service.js
+++ application/grpc-crud-service.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const grpc = require('@grpc/grpc-js');
-
 module.exports = class GrpcCrudService {
   constructor(repository) {
     this.repository = repository;
@@ -15,50 +13,19 @@ module.exports = class GrpcCrudService {
 
   create(call, callback) {
     this.repository.create(call.request)
-      .then(() => callback(null, grpc.Empty()))
+      .then(() => callback(null, {}))
       .catch((err) => callback(err));
   }
 
   update(call, callback) {
     this.repository.update(call.request)
-      .then(() => callback(null, grpc.Empty()))
+      .then(() => callback(null, {}))
       .catch((err) => callback(err));
   }
 
   delete(call, callback) {
     this.repository.delete(call.request.id)
-      .then(() => callback(null, grpc.Empty()))
+      .then(() => callback(null, {}))
       .catch((err) => callback(err));
   }
 };
-
-// function GrpcCrudService(repository) {
-//   console.log('Inited');
-//   this.repository = repository;
-// }
-//
-// GrpcCrudService.prototype.read = (call, callback) => {
-//   this.repository.read(call.request.id)
-//     .then((record) => callback(null, record))
-//     .catch((err) => callback(err));
-// };
-//
-// GrpcCrudService.prototype.create = (call, callback) => {
-//   this.repository.create(call.request)
-//     .then(() => callback(null, grpc.Empty()))
-//     .catch((err) => callback(err));
-// };
-//
-// GrpcCrudService.prototype.update = (call, callback) => {
-//   this.repository.update(call.request)
-//     .then(() => callback(null, grpc.Empty()))
-//     .catch((err) => callback(err));
-// };
-//
-// GrpcCrudService.prototype.delete = (call, callback) => {
-//   this.repository.delete(call.request.id)
-//     .then(() => callback(null, grpc.Empty()))
-//     .catch((err) => callback(err));
-// };
-//
-// module.exports = GrpcCrudService;
